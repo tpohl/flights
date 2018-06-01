e11d4caab972da14288d018d455ad6f8b78c2d21
Thorsten Pohl <thpohl@gmail.com>
2018-06-01 23:11:12 +0200

    temp - non working autocomplete function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 
+import * as flightAutoComplete from './autocomplete/flight-autocomplete.server.service';
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -10,14 +12,22 @@ import * as functions from 'firebase-functions';
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
 exports.flightNoUppercase = functions.database.ref('/users/{userId}/flights/{flightId}/flightno')
-    .onUpdate((change, context) => {
-      // Grab the current value of what was written to the Realtime Database.
-      const originalFlight = change.after.val();
-      console.log('Uppercasing Flight No', context.params.userId, context.params.flightId, originalFlight);
+  .onUpdate((change, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const originalFlight = change.after.val();
+    console.log('Uppercasing Flight No', context.params.userId, context.params.flightId, originalFlight);
 
-      const uppercase = originalFlight.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return change.after.ref.set(uppercase);
+    const uppercase = originalFlight.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return change.after.ref.set(uppercase);
+  });
+exports.flightAutoComplete = functions.database.ref('/users/{userId}/flights/{flightId}/flightno')
+  .onUpdate((change, context) => {
+    const flightNo = change.after.val();
+    flightAutoComplete.autocomplete(flightNo).then(flight => {
+      change.after.ref.parent["autocompletes"] = flight;
     });
+
+  });
