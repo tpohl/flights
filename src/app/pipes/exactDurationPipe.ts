7af72578a059fc2448c0ee9a8ae6639df0266484
Thorsten Pohl <thpohl@gmail.com>
2018-09-24 22:14:31 +0200

    feat - horizontal form and exact duration

--- src/app/pipes/exactDurationPipe.ts
+++ src/app/pipes/exactDurationPipe.ts
@@ -8,6 +8,7 @@ export class ExactDurationPipe implements PipeTransform {
       throw new Error('DurationPipe: missing required time unit argument');
     }
     const duration = moment.duration(value, args[0] as moment.unitOfTime.DurationConstructor);
+    console.log("DURARION", value, args[0]);
     //return duration.get('hours') + ':' + duration.get('minutes');
     return moment.utc(duration.asMilliseconds()).format('HH:mm');
   }
