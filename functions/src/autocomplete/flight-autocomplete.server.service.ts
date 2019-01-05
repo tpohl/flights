2503ba5e9a8cc89a0e0e42c646bfcbcf8c2758c7
Thorsten Pohl <thpohl@gmail.com>
2019-01-05 18:16:32 +0100

    feat - removes distance calcutation from autocomplete.

--- functions/src/autocomplete/flight-autocomplete.server.service.ts
+++ functions/src/autocomplete/flight-autocomplete.server.service.ts
@@ -7,7 +7,7 @@ import * as moment from 'moment';
 import { filter, map, tap, flatMap } from "rxjs/operators";
 import { from, zip, Observable } from "rxjs";
 import * as simpleoauth2 from 'simple-oauth2';
-import { DataSnapshot } from '../../node_modules/firebase-functions/lib/providers/database';
+import { DataSnapshot } from 'firebase-functions/lib/providers/database';
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const config = functions.config();
@@ -107,28 +107,6 @@ const toFlight = function (lhApiFlight: any, index) {
   return flight;
 }
 
-const calculateDistance = function (lat1: number, long1: number, lat2: number, long2: number) {
-  const p = 0.017453292519943295;    // Math.PI / 180
-  const c = Math.cos;
-  const a = 0.5 - c((lat1 - lat2) * p) / 2 + c(lat2 * p) * c((lat1) * p) * (1 - c(((long1 - long2) * p))) / 2;
-  const dis = Math.round((12742 * Math.asin(Math.sqrt(a)))); // 2 * R; R = 6371 km
-  return dis;
-}
-const addDistance = function (flight: Flight) {
-  console.log('Adding Distance', flight);
-  const fromAirport$ = from(admin.database().ref('/airports/' + flight.from).once('value')) as Observable<DataSnapshot>;
-  const toAirport$ = from(admin.database().ref('/airports/' + flight.to).once('value')) as Observable<DataSnapshot>;
-
-  return zip(fromAirport$, toAirport$)
-    .pipe(map(ap => {
-      const ap1 = ap[0].val() as Airport;
-      const ap2 = ap[1].val() as Airport;
-      flight.distance = calculateDistance(ap1.latitude, ap1.longitude, ap2.latitude, ap2.longitude);
-      return flight;
-    }));
-
-}
-
 const FlightAutoCompleter = {
   autocomplete: function (flightNo, dateStr: string) {
     // Default to current Date.
@@ -138,32 +116,32 @@ const FlightAutoCompleter = {
 
 
     const flight$ = getLhApiToken()
-      .pipe(map(apiTokenObj => apiTokenObj.token.access_token))
-      .pipe(flatMap(apiToken =>
-        RxHR.get('https://api.lufthansa.com/v1/operations/flightstatus/' + flightNo + '/' + date,
-          {
-            headers: {
-              'User-Agent': 'request',
-              'Accept': 'application/json',
-              'Authorization': 'Bearer ' + apiToken
-            },
-            json: true
-          })
-      ))
-      //.pipe(tap(data => console.log('DATA', data)))
-      .pipe(filter(data => data.response.statusCode === 200))
-      .pipe(map(data => data.body))
-      //.pipe(tap(body => console.log('BODY', body)))
-      .pipe(map(apiResponse => apiResponse.FlightStatusResource.Flights.Flight))
-      .pipe(tap(console.log))
-      .pipe(map(toFlight))
-      .pipe(flatMap(addDistance))
-      .pipe(flatMap((flight) => loadAircraftType(flight.aircraftType)
-        .pipe(map(acType => {
-          flight.aircraftType = acType;
-          return flight;
-        }))
-      ))
+      .pipe(
+        map(apiTokenObj => apiTokenObj.token.access_token),
+        flatMap(apiToken =>
+          RxHR.get('https://api.lufthansa.com/v1/operations/flightstatus/' + flightNo + '/' + date,
+            {
+              headers: {
+                'User-Agent': 'request',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + apiToken
+              },
+              json: true
+            })
+        ),
+        tap(data => console.trace('DATA', data)),
+        filter(data => data.response.statusCode === 200),
+        map(data => data.body),
+        map(apiResponse => apiResponse.FlightStatusResource.Flights.Flight),
+        tap(console.debug),
+        map(toFlight),
+        //flatMap(addDistance),
+        flatMap((flight) => loadAircraftType(flight.aircraftType)
+          .pipe(map(acType => {
+            flight.aircraftType = acType;
+            return flight;
+          }))
+        ))
 
       ;
 
