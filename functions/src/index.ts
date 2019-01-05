1d1606a5a3a0c35a74e1ce1b427f537f087e8915
Thorsten Pohl <thpohl@gmail.com>
2019-01-05 18:40:14 +0100

    chore - group our functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,11 @@
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-/*
-exports.flightNoUppercase = functions.database.ref('/users/{userId}/flights/{flightId}/flightno')
-  .onUpdate((change, context) => {
-    // Grab the current value of what was written to the Realtime Database.
-    const originalFlight = change.after.val();
-    console.log('Uppercasing Flight No', context.params.userId, context.params.flightId, originalFlight);
-
-    const uppercase = originalFlight.toUpperCase();
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to the Firebase Realtime Database.
-    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-    return change.after.ref.set(uppercase);
-  });*/
-
-//export {flightAutoComplete, flightAutoCompleteOnCreate} from './autocomplete';
+// Import all the functions
 import autocompleteFunctions from './autocomplete';
 import durationFunctions from './duration';
 import distanceFunctions from './distance';
 
-export default { ...autocompleteFunctions, ...durationFunctions, ...distanceFunctions };
+
+// Export our functions
+export const duration = durationFunctions;
+export const distance = distanceFunctions;
+export const autocomplete = autocompleteFunctions;
+
