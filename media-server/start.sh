ef4d23ed4255334b6299ef875a84dc82dc1e757f
POHL, THORSTEN <thorsten.pohl@lht.dlh.de>
2024-07-25 21:24:18 +0200

    feat - some design

--- media-server/start.sh
+++ media-server/start.sh
@@ -9,5 +9,5 @@ docker run -d --name flights-media-server --restart always  \
     -v /home/th0r5ti/software/flights-media-server/airport-cache:/deployments/imagecache/airports \
     -e "VIRTUAL_HOST=flights-media.pohl.rocks" -e "PORT=8080" -e "VIRTUAL_PORT=8080" \
     -e "LETSENCRYPT_HOST=flights-media.pohl.rocks" -e "LETSENCRYPT_EMAIL=thpohl@gmail.com" \
-    -e "GOOGLE_PLACES_API_KEY=" \
+    -e "GOOGLE_PLACES_API_KEY=AIzaSyCSYwK2nL-Snh3w6ZVkI6IW3OdfRjuiEFc" \
     flights-media-server
