6c432c769e18e49e697c8ad3eacaf2cd89bc2b14
Thorsten <thpohl@gmail.com>
2025-11-10 12:46:07 +0100

    fix: update flight-edit component to use ReplaySubject for flights and improve null safety

--- app/src/app/flight-edit/flight-edit.component.ts
+++ app/src/app/flight-edit/flight-edit.component.ts
@@ -1,6 +1,6 @@
 import { Location, LocationStrategy, PathLocationStrategy, CommonModule } from '@angular/common';
 import { AirportService } from '../services/airport.service';
-import { BehaviorSubject, filter, Observable, of, Subscription } from 'rxjs';
+import { BehaviorSubject, filter, Observable, of, ReplaySubject, Subject, Subscription } from 'rxjs';
 
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
@@ -26,11 +26,12 @@ import { FlightsService, SaveResultType } from '../services/flights.service';
 import User = firebase.User;
 import { TRAVEL_CLASSES } from '../seat-info/seat-info.component';
 import { ExactDurationPipe } from '../pipes/exactDurationPipe';
+import { FlightTileComponent } from '../flight-tile/flight-tile.component';
 
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FormsModule, FlightStatsComponent, RelativeTimePipe, FlightDistancePipe, CesiumDirective, ExactDurationPipe, RouterLink],
+  imports: [CommonModule, FormsModule, FlightTileComponent, FlightStatsComponent, RelativeTimePipe, FlightDistancePipe, CesiumDirective, ExactDurationPipe, RouterLink],
   selector: 'app-flight-edit',
   providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
   templateUrl: './flight-edit.component.html',
@@ -51,7 +52,7 @@ export class FlightEditComponent implements OnInit, OnDestroy {
   @Input()
   flightId: string | undefined;
 
-  flight: Flight = new Flight();
+  flight: Flight | null = null;
   user: User | null = null;
 
   departureTime = '00:00';
@@ -63,15 +64,14 @@ export class FlightEditComponent implements OnInit, OnDestroy {
 
   fromAirport$: BehaviorSubject<Airport | null> = new BehaviorSubject<Airport | null>(null);
   toAirport$: BehaviorSubject<Airport | null> = new BehaviorSubject<Airport | null>(null);
+  flightsForMap$: Subject<Flight[]> = new ReplaySubject<Flight[]>(1);
+  
   private subs = new Subscription();
 
-  flightsForMap() {
-    return of([this.flight]);
-  }
+
+
 
   ngOnInit() {
-
-    console.log(TRAVEL_CLASSES, this.TRAVEL_CLASSES_LIST);
     this.subs.add(this.afAuth.user.subscribe(user => {
       this.user = user;
 
@@ -100,31 +100,32 @@ export class FlightEditComponent implements OnInit, OnDestroy {
   }
 
   selectDepartureTime() {
+    if (!!this.flight) {
+      const time = this.departureTime;
 
-    const time = this.departureTime;
-
-    const dateWithWithTime = DayJS(this.flight.date).format('YYYY-MM-DD') + 'T' + time;
-    this.fromAirport$
-      .pipe(filter(ap => !!ap && !!ap.timezoneId))
-      .subscribe(ap => {
-        this.flight.departureTime = DayJS.tz(dateWithWithTime, ap!!.timezoneId).clone().tz('UTC').format(); // '2013-06-01T00:00:00',
-      });
+      const dateWithWithTime = DayJS(this.flight.date).format('YYYY-MM-DD') + 'T' + time;
+      this.fromAirport$
+        .pipe(filter(ap => !!ap && !!ap.timezoneId))
+        .subscribe(ap => {
+          this.flight!.departureTime = DayJS.tz(dateWithWithTime, ap!!.timezoneId).clone().tz('UTC').format(); // '2013-06-01T00:00:00',
+        });
+    }
   }
 
   selectArrivalTime() {
+    if (!!this.flight) {
+      const time = this.arrivalTime;
 
-    const time = this.arrivalTime;
-
-    const dateWithWithTime = DayJS(this.flight.date).format('YYYY-MM-DD') + 'T' + time;
-    this.toAirport$
-      .pipe(filter(ap => !!ap && !!ap.timezoneId))
-      .subscribe(ap => {
-        this.flight.arrivalTime = DayJS.tz(dateWithWithTime, ap!!.timezoneId).tz('UTC').format(); // '2013-06-01T00:00:00',
-        if (this.flight.arrivalTime < this.flight.departureTime) { // When the arrival is BEFORE the Departure, then we add a day.
-          this.flight.arrivalTime = DayJS(this.flight.arrivalTime).add(1, 'days').clone().tz('UTC').format();
-        }
-    });
-
+      const dateWithWithTime = DayJS(this.flight.date).format('YYYY-MM-DD') + 'T' + time;
+      this.toAirport$
+        .pipe(filter(ap => !!ap && !!ap.timezoneId))
+        .subscribe(ap => {
+          this.flight!.arrivalTime = DayJS.tz(dateWithWithTime, ap!!.timezoneId).tz('UTC').format(); // '2013-06-01T00:00:00',
+          if (this.flight!.arrivalTime < this.flight!.departureTime) { // When the arrival is BEFORE the Departure, then we add a day.
+            this.flight!.arrivalTime = DayJS(this.flight!.arrivalTime).add(1, 'days').clone().tz('UTC').format();
+          }
+      });
+    }
   }
 
   loadFlight(flightId: string) {
@@ -135,6 +136,7 @@ export class FlightEditComponent implements OnInit, OnDestroy {
           this.objectRef = flight._objectReference;
           this.loadFromAirport(this.flight.from);
           this.loadToAirport(this.flight.to);
+          this.flightsForMap$.next([flight]);
         } else {
           console.error('Flight not found: ' + flightId);
           this.router.navigateByUrl('/flights');
@@ -144,7 +146,9 @@ export class FlightEditComponent implements OnInit, OnDestroy {
   }
 
   selectedDate(momentDate: DayJS.Dayjs) {
-    this.flight.date = momentDate.format('YYYY-MM-DD');
+    if (!!this.flight) {
+      this.flight.date = momentDate.format('YYYY-MM-DD');
+    }
   }
 
   loadAirport(code: String): Observable<Airport | null> {
@@ -168,24 +172,28 @@ export class FlightEditComponent implements OnInit, OnDestroy {
   }
 
   autocomplete(): void {
-    this.flight.needsAutocomplete = (!this.flight.needsAutocomplete);
-    return this.save();
+    if (!!this.flight) {
+      this.flight.needsAutocomplete = (!this.flight.needsAutocomplete);
+      this.save();
+    }
   }
 
   save(): void {
-    const sub = this.flightsService.saveFlight(this.flight)
-      .subscribe(result => {
-        if (!!result && result.type === SaveResultType.CREATED) {
-          const flightId = result.flightId;
-          this.loadFlight(flightId);
-          this.location.replaceState('flight/' + flightId);
-        }
-        sub.unsubscribe();
-      });
+    if (!!this.flight) {
+      const sub = this.flightsService.saveFlight(this.flight)
+        .subscribe(result => {
+          if (!!result && result.type === SaveResultType.CREATED) {
+            const flightId = result.flightId;
+            this.loadFlight(flightId);
+            this.location.replaceState('flight/' + flightId);
+          }
+          sub.unsubscribe();
+        });
+    }
   }
 
   delete(): void {
-    if (this.objectRef) {
+    if (!!this.flight && !!this.objectRef) {
       this.flight._deleted = true;
 
       const sub = this.flightsService.saveFlight(this.flight)
