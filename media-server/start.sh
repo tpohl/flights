7c04275c2e9f2f02673d2cf038ecee66131a323e
Thorsten <thpohl@gmail.com>
2026-01-15 21:23:18 +0100

    chore - move secret to secret.env - key rotated

--- media-server/start.sh
+++ media-server/start.sh
@@ -11,5 +11,5 @@ docker run -d --name flights-media-server --restart always  \
     -v /home/th0r5ti/software/flights-media-server/airline-cache:/deployments/imagecache/airlines \
     -e "VIRTUAL_HOST=flights-media.pohl.rocks" -e "PORT=8080" -e "VIRTUAL_PORT=8080" \
     -e "LETSENCRYPT_HOST=flights-media.pohl.rocks" -e "LETSENCRYPT_EMAIL=thpohl@gmail.com" \
-    -e "GOOGLE_PLACES_API_KEY=AIzaSyCSYwK2nL-Snh3w6ZVkI6IW3OdfRjuiEFc" \
+    --env-file secrets.env \
     flights-media-server
